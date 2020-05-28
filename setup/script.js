32198f0986404c70abb1183dbf7ca8e05774ad6c
Carole Girard <kca.girard@gmail.com>
2020-05-28 18:22:35 +0200

    first commit

---
+++ setup/script.js
@@ -0,0 +1,14 @@
+document.getElementsByClassName(cookie).addEventListener("click",
+function () {
+    if(document.getElementsByClassName(cookie)) {
+        document.getElementsByClassName(cookie) = document.getElementsByClassName(croc)
+    }
+    if (document.getElementsByClassName(croc)) {
+        document.getElementsByClassName(croc) = document.getElementsByClassName(croc).remove
+    }
+    else {
+        document.getElementsByClassName(cookie) = document.getElementsByClassName(cookie).remove
+    }
+
+}
+)
